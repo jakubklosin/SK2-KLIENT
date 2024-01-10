aff10204dc89958f565187b2ccb915437b61a548
wiktorbromberek <wiktorbromberek@gmail.com>
2024-01-10 02:19:51 +0100

    update

--- client/src/main/java/org/example/GameSession.java
+++ client/src/main/java/org/example/GameSession.java
@@ -169,7 +169,6 @@ public class GameSession {
             JSONObject answerJson = new JSONObject();
             answerJson.put("action","answering");
             answerJson.put("kod pokoju", roomCode);
-            answerJson.put("action", "answering");
             answerJson.put("nickname", playerName);
             answerJson.put("numer pytania", currentQuestionIndex + 1); // Zakładając, że numeracja pytań zaczyna się od 1
             answerJson.put("odpowiedz", chosenAnswer);
